71782f3b1d592c8ab0a25dfea576479f50f780bb
mysticblackmask <black.mask0209@outlook.com>
2024-12-09 01:39:28 -0800

    get all data

--- sku_manager/urls.py
+++ sku_manager/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import (
+    read_skus
     create_sku,
 )
 
 urlpatterns = [
+    path('read/', read_skus, name='read_skus'),
     path('create/', create_sku, name='create_sku'),
 ]
--- sku_manager/views.py
+++ sku_manager/views.py
@@ -5,6 +5,14 @@ from rest_framework.response import Response
 from .models import MedicationSKU
 from .serializers import MedicationSKUSerializer
 
+@api_view(['GET'])
+def read_skus(request):
+    if request.method == 'GET':
+        skus = MedicationSKU.objects.all()
+        if not skus.exists():
+            return Response({'message': 'No SKUs found'}, status=404)
+        serializer = MedicationSKUSerializer(skus, many=True)
+        return Response(serializer.data)
 
 @api_view(['POST'])
 def create_sku(request):
