b5eff213e5f89ba718860254be117f96f76733d9
mysticblackmask <black.mask0209@outlook.com>
2024-12-09 02:08:01 -0800

    setting for deploying

--- axmed/settings.py
+++ axmed/settings.py
@@ -26,9 +26,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'https://h-axmed-medication.onrender.com'
+]
 
 
 # Application definition
